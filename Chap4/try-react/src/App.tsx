5b1ded5ac362649b6196174d27bfceb103d3d2ca
José <48959374+jsfben2014@users.noreply.github.com>
2022-04-27 00:31:12 -0300

    Update App.tsx
    <Routes> is the new <Switch>
    The <Routes > is the new <Switch> element now. It is very much similar to <Switch> with some new features.

--- Chap4/try-react/src/App.tsx
+++ Chap4/try-react/src/App.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import "./App.css";
 import Home from "./Home";
 import AnotherScreen from './AnotherScreen';
-import { Switch, Route } from "react-router";
+import {Route, Routes} from "react-router";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Switch>
-          <Route exact={true} path="/" component={Home}></Route>
-          <Route path="/another" component={AnotherScreen}></Route>
-        </Switch>
+      <Routes>
+      <Route  path="/" element={<Home />} />
+      <Route path="/another" element={<AnotherScreen />} />
+      </Routes>
       </header>
     </div>
   );
 }
-
 export default App;
